acd3ff234c37ec2c59ad7c901548f602583fa4a5
CH <cedwards2809@gmail.com>
2026-02-23 11:32:11 -0500

    added the ability to parse variables

--- main.py
+++ main.py
@@ -1,9 +1,27 @@
 class Interpreter:
     def __init__(self):
         self.stack = []
+        self.enviorment = {}
     
     def LOAD_VALUE(self, value):
         self.stack.append(value)
+    
+    def STORE_NAME(self, name):
+        val = self.stack.pop()
+        self.enviorment[name] = val
+
+    def LOAD_NAME(self, name):
+        val = self.enviorment[name]
+        self.stack.append(val)
+    
+    def parse_argument(self, instruction, argument, what_to_run):
+        numbers = ["LOAD_VALUE"]
+        names = ["LOAD_VALUE","STORE_NAME"]
+        if instruction in numbers:
+            argument = what_to_run["numbers"][argument]
+        elif instruction in names:
+            argument = what_to_run["names"][argument]
+        return argument
 
     def PRINT_ANSWER(self):
         ans = self.stack.pop()
@@ -20,23 +38,30 @@ class Interpreter:
         numbers = what_to_run["numbers"]
         for each_step in instructions:
             instruction, argument = each_step
+            argument = self.parse_argument(instruction, argument, what_to_run)
             if instruction == "LOAD_VALUE":
-                number = numbers[argument]
-                self.LOAD_VALUE(number)
+                self.LOAD_VALUE(argument)
             elif instruction == "PRINT_ANSWER":
                 self.PRINT_ANSWER()
             elif instruction == "ADD_TWO_VALUES":
                 self.ADD_TWO_VAlUES()
+            elif instruction == "STORE_NAME":
+                self.STORE_NAME(argument)
+            elif instruction == "LOAD_NAME":
+                self.LOAD_NAME(argument)
 interpreter = Interpreter()
 
 what_to_run = {
-    "instructions":[("LOAD_VALUE", 0),
+    "instructions": [("LOAD_VALUE", 0),
+                         ("STORE_NAME", 0),
                          ("LOAD_VALUE", 1),
+                         ("STORE_NAME", 1),
+                         ("LOAD_NAME", 0),
+                         ("LOAD_NAME", 1),
                          ("ADD_TWO_VALUES", None),
-                         ("LOAD_VALUE", 2),
-                         ("ADD_TWO_VALUES", None),
-                         ("PRINT_ANSWER", None)]
-    ,"numbers": [7,5,8]
+                         ("PRINT_ANSWER", None)],
+        "numbers": [1, 2],
+        "names":   ["a", "b"]
 
 }
 
