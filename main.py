0a25105b7f8b5651b9668b322d5dd3d2654f396e
CH <cedwards2809@gmail.com>
2026-02-23 11:09:20 -0500

    creates a skeleton struction of the interprater that can add and print values

--- main.py
+++ main.py
@@ -1,7 +1,48 @@
 class Interpreter:
-    def __int__(self):
+    def __init__(self):
         self.stack = []
     
     def LOAD_VALUE(self, value):
-        self.
+        self.stack.append(value)
+
+    def PRINT_ANSWER(self):
+        ans = self.stack.pop()
+        print(ans)
+
+    def ADD_TWO_VAlUES(self):
+        first_num = self.stack.pop()
+        second_num = self.stack.pop()
+        total = first_num + second_num
+        self.stack.append(total)
+    
+    def run_code(self, what_to_run):
+        instructions = what_to_run["instructions"]
+        numbers = what_to_run["numbers"]
+        for each_step in instructions:
+            instruction, argument = each_step
+            if instruction == "LOAD_VALUE":
+                number = numbers[argument]
+                self.LOAD_VALUE(number)
+            elif instruction == "PRINT_ANSWER":
+                self.PRINT_ANSWER()
+            elif instruction == "ADD_TWO_VALUES":
+                self.ADD_TWO_VAlUES()
+interpreter = Interpreter()
+
+what_to_run = {
+    "instructions":[("LOAD_VALUE", 0),
+                         ("LOAD_VALUE", 1),
+                         ("ADD_TWO_VALUES", None),
+                         ("LOAD_VALUE", 2),
+                         ("ADD_TWO_VALUES", None),
+                         ("PRINT_ANSWER", None)]
+    ,"numbers": [7,5,8]
+
+}
+
+interpreter.run_code(what_to_run)
+    
+
+
+
     
