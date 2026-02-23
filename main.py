34571cc75376c9298d4c14fabc70303122fd6eb1
CH <cedwards2809@gmail.com>
2026-02-23 14:21:00 -0500

    updated exicution code

--- main.py
+++ main.py
@@ -43,24 +43,6 @@ class Interpreter:
                     bytecode_method()
                 else:
                     bytecode_method(argument)
-    """
-    def run_code(self, what_to_run):
-        instructions = what_to_run["instructions"]
-        numbers = what_to_run["numbers"]
-        for each_step in instructions:
-            instruction, argument = each_step
-            argument = self.parse_argument(instruction, argument, what_to_run)
-            if instruction == "LOAD_VALUE":
-                self.LOAD_VALUE(argument)
-            elif instruction == "PRINT_ANSWER":
-                self.PRINT_ANSWER()
-            elif instruction == "ADD_TWO_VALUES":
-                self.ADD_TWO_VAlUES()
-            elif instruction == "STORE_NAME":
-                self.STORE_NAME(argument)
-            elif instruction == "LOAD_NAME":
-                self.LOAD_NAME(argument)
-    """
 
 
         
