1b88ee7a2adc21310b39c2eb59106b3aea8d2d3f
CH <cedwards2809@gmail.com>
2026-02-27 09:15:19 -0500

    commit

--- Bytecode.py
+++ Bytecode.py
@@ -9,22 +9,21 @@ class VirtualMachine(object):
         self.last_exception = None
     
     def run_code(self, code, global_names=None, local_names=None):
-        frame = self.make_frame(code, global_names == global_names, local_names == local_names)
+        frame = self.make_frame(code, {}, global_names, local_names)
         self.run_frame(frame) 
 
     def make_frame(self, code, callargs={}, global_names=None, local_names=None):
-        if global_names is not None and local_names is not None:
-            local_names = global_names
-        elif self.frames:
-            global_names = self.frame.global_names
-            local_names = {}
-        else:
-            global_names = local_names = {
-                '__builtins__': __builtins__,
-                '__name__': '__main__',
-                '__doc__': None,
-                '__package__': None,
-            }
+        if global_names is None or local_names is None:
+            if self.frames:
+                global_names = self.frame.global_names
+                local_names = {}
+            else:
+                global_names = local_names = {
+                    '__builtins__': __builtins__,
+                    '__name__': '__main__',
+                    '__doc__': None,
+                    '__package__': None,
+                }
         local_names.update(callargs)
         frame = Frame(code, global_names, local_names, self.frame)
         return frame
@@ -40,7 +39,7 @@ class VirtualMachine(object):
         else:
             self.frame = None
 
-    def run_frame(self):
+    def run_frame(self, frame):
         pass
     
 
