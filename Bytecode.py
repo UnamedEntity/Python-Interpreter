c9b3f71bba804ca4a2253eb3089d309d23701b2a
CH <cedwards2809@gmail.com>
2026-02-27 10:02:44 -0500

    Added ability to parse functions

--- Bytecode.py
+++ Bytecode.py
@@ -1,3 +1,7 @@
+from importlib.metadata.diagnose import inspect
+from wsgiref import types
+import dis
+
 class VirtualMachineError(Exception):
     pass
 
@@ -41,6 +45,49 @@ class VirtualMachine(object):
 
     def run_frame(self, frame):
         pass
+
+    def top(self):
+        return self.frame.stack[-1]
+
+    def pop(self):
+        return self.frame.stack.pop()
+
+    def push(self, *vals):
+            self.frame.stack.extend(vals)
+
+    def popn(self, n):
+        if n:
+            ret = self.frame.stack[-n:]
+            self.frame.stack[-n:] = []
+            return ret
+        else:
+            return []
+    def parse_byte_and_args(self):
+        f = self.frame
+        opoffset = f.last_instruction
+        byteCode = f.code.co_code[opoffset]
+        f.last_instruction += 1
+        byte_name = dis.opname[byteCode]
+        if byteCode >= dis.HAVE_ARGUMENT:
+            arg = f.code.co_code[f.last_instruction:f.last_instruction+2]  
+            f.last_instruction += 2   # advance the instruction pointer
+            arg_val = arg[0] + (arg[1] * 256)
+            if byteCode in dis.hasconst:   # Look up a constant
+                arg = f.code.co_consts[arg_val]
+            elif byteCode in dis.hasname:  # Look up a name
+                arg = f.code.co_names[arg_val]
+            elif byteCode in dis.haslocal: # Look up a local name
+                arg = f.code.co_varnames[arg_val]
+            elif byteCode in dis.hasjrel:  # Calculate a relative jump
+                arg = f.last_instruction + arg_val
+            else:
+                arg = arg_val
+            argument = [arg]
+        else:
+            argument = []
+
+        return byte_name, argument
+
     
 
 class Frame(object):
@@ -59,3 +106,40 @@ class Frame(object):
         
         self.last_instruction = 0
         self.block_stack = []
+
+class Function(object):
+    __slots__ = [
+        'func_code', 'func_name', 'func_defaults', 'func_globals',
+        'func_locals', 'func_dict', 'func_closure',
+        '__name__', '__dict__', '__doc__',
+        '_vm', '_func',
+    ]
+
+    def __init__(self, name, code, globs, defaults, closure, vm):
+        self._vm = vm
+        self.func_code = code
+        self.func_name = self.__name__ = name or code.co_name
+        self.func_defaults = tuple(defaults)
+        self.func_globals = globs
+        self.func_locals = self._vm.frame.f_locals
+        self.__dict__ = {}
+        self.func_closure = closure
+        self.__doc__ = code.co_consts[0] if code.co_consts else None
+        kw = {
+            'argdefs': self.func_defaults,
+        }
+        if closure:
+            kw['closure'] = tuple(make_cell(0) for _ in closure)
+        self._func = types.FunctionType(code, globs, **kw)
+
+    def __call__(self, *args, **kwargs):
+        """When calling a Function, make a new frame and run it."""
+        callargs = inspect.getcallargs(self._func, *args, **kwargs)
+        frame = self._vm.make_frame(
+            self.func_code, callargs, self.func_globals, {}
+        )
+        return self._vm.run_frame(frame)
+
+def make_cell(value):
+    fn = (lambda x: lambda: x)(value)
+    return fn.__closure__[0]
